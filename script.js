1866b8bf2cc248660890efbbfa3e2a4d6b315b37
inserveofgod <inserveofgod@gmail.com>
2022-07-19 13:38:17 +0300

    crud, modify, clone, iterating, filter, form validation actions are done

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-"use strict";
+"use strict"
 
 $(document).ready(function () {
+    /*
+    * some useful functions
+    * */
+
     $('#paragraph').hide(0).addClass('text-secondary').fadeIn(1000)
     // $('#list').find('li').addClass('text-danger')
     // $('#list').children('li').addClass('text-danger')
@@ -14,18 +18,94 @@ $(document).ready(function () {
     // alert($('#form').html())
     // alert($('#form').val())
 
-    $('form').on('submit', (element) => {
+    /*
+    * reading & writing values from elements
+    * */
+
+    $('#firstForm').on('submit', (element) => {
         element.preventDefault()
 
         if ($('#username').val() === 'David123') {
-            $('form small.text-danger').text('')
-            $('form small.text-success').text("Welcome")
+            $('#firstForm small.text-danger').text('')
+            $('#firstForm small.text-success').text("Welcome")
         } else {
-            $('form small.text-success').text('')
-            $('form small.text-danger').text("Invalid username")
+            $('#firstForm small.text-success').text('')
+            $('#firstForm small.text-danger').text("Invalid username")
         }
     })
 
-    const cols = $('table th').length
-    $('table').html("<tr><td>2</td><td>InServeOfGod</td><td>*****</td></tr>")
+    /*
+    * adding & inserting elements
+    * */
+
+    // const cols = $('table th').length
+    $('table').append('<tr><td>2</td><td>InServeOfGod</td><td>*****</td></tr>')
+    // append, prepend, after, before
+
+    /*
+    * removing elements
+    * */
+
+    $('#removeBtn').on('click', () => {
+        $('#removeBtn').remove()
+    })
+
+    /*
+    * modify the attributes
+    * attr, prop
+    * */
+
+    const google = $('#google')
+    google.attr('href', "https://www.google.com").removeAttr('target')
+
+    if (!google.hasClass('link-danger')) {
+        google.addClass('link-danger')
+    }
+
+    /*
+    * clone the elements
+    * */
+
+    $('#cloneBtn').click(() => {
+        $('.cloneImg').clone().insertAfter('.cloneImg')
+    })
+
+    /*
+    * iterating with each function
+    * */
+
+    $('#list li').each((index, value) => {
+        $(value).addClass('text-black')
+    })
+
+    /*
+    * filtering elements : eq, filter, has, not
+    * */
+
+    // $('#filters a').eq(2).removeClass('link-info').addClass('link-danger')
+    // $('#filters a').filter('.link-primary').removeClass('link-primary').addClass('link-danger')
+    // $('#filters a').not('.link-primary').removeAttr('class').addClass('link-secondary')
+    if ($('#filters').has('a')) {
+        $('#filters a').each((index, value) => {
+            $(value).attr('target', '_blank')
+        })
+    }
+
+    /*
+    * data API
+    * */
+
+    // todo : research it
+
+    /*
+    * form validation
+    * */
+
+    $('#secondForm').on('submit', (element) => {
+        element.preventDefault()
+
+        if (!$("#secondForm input[type='radio']").is(':checked')) {
+            alert('please select any gender')
+        }
+    })
 })
